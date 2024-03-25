fe30644b61ab6bf0a2b786f65bd84411c881fa95
Habimana Nyiringoma Tresor <tresornyiringoma@gmail.com>
2024-03-25 15:13:03 +0200

    react todo app

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenSquare, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-export const Todo = ({task, toggleComplete}) => {
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+  const handleTextClick = () => {
+    toggleComplete(task.id);
+  };
+
+  const handleEdit = () => {
+    editTodo(task.id);
+  };
+
+  const handleDelete = () => {
+    deleteTodo(task.id);
+  };
+
   return (
-    <div className = 'Todo'>
-      <p onClick={() => toggleComplete(task.id)} className ={`${task.completed ? 'completed': ""}`}>{task.task}</p>
+    <div className='Todo'>
+      <p
+        onClick={handleTextClick}
+        className={`todo-text ${task.completed ? 'completed' : ''}`}
+      >
+        {task.task}
+      </p>
       <div>
-        <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faPenSquare} onClick={handleEdit} />
+        <FontAwesomeIcon icon={faTrash} onClick={handleDelete}/>
       </div>
     </div>
-  )
-}
+  );
+};
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -2,27 +2,67 @@ import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
 import { Todo } from './Todo';
 import { v4 as uuidv4 } from 'uuid';
+import '../styles/style.css';
+import { EditTodoForm } from './EditForm';
 uuidv4();
 
 export const TodoWrapper = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-  const addTodo = todo => {
-    setTodos([...todos, {id: uuidv4(), task: todo,
-      completed: false, isEditing: false}])
-      console.log(todos)
-  }
-    const toggleComplete = id => {
-      setTodos(todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed} : todo))
-    }
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const editTask = (EditedtaskText, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task: EditedtaskText, isEditing: false } : todo
+      )
+    );
+  };
+
+
   return (
     <div>
       <h1>Here's My Todo List!</h1>
-      <TodoForm addTodo={addTodo}/>
+      <TodoForm addTodo={addTodo} />
       {todos.map((todo, index) => (
-        <Todo task={todo} key={index} 
-        toggleComplete={toggleComplete}/>
+        todo.isEditing ? (
+        <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+        <Todo
+          task={todo}
+          key={index}
+          toggleComplete={toggleComplete}
+          editTodo={editTodo}
+          deleteTodo={deleteTodo}
+        />
+        )
       ))}
     </div>
-  )
-}
+  );
+};
+
